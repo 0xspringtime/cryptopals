002b08a36bb65a939b6175297f4e03890a81c2cf
0xspringtime <110655352+0xspringtime@users.noreply.github.com>
2023-01-08 20:23:28 -0500

    Add files via upload

--- 1-8.py
+++ 1-8.py
@@ -1,1 +1,19 @@
 #1-8#1-8#1-8#1-8#1-8#1-8#1-8#1-8
+# Load the hex-encoded ciphertexts from the file
+with open("8.txt", "r") as f:
+    ciphertexts = f.readlines()
+
+# Iterate over the ciphertexts
+for ciphertext in ciphertexts:
+    # Convert the ciphertext from hex to bytes
+    ciphertext = bytearray.fromhex(ciphertext.strip())
+
+    # Split the ciphertext into blocks of 16 bytes
+    blocks = [ciphertext[i:i+16] for i in range(0, len(ciphertext), 16)]
+    
+    blocks = [tuple(block) for block in blocks]
+
+
+    # Check if there are any repeating blocks
+    if len(blocks) != len(set(blocks)):
+        print("ECB mode detected in ciphertext:", ciphertext.hex())
